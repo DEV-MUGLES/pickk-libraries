4461fb1e0e7fb7921ae15b41356c831ec6fd3839
greatSumini <greatSumini@gmail.com>
2021-10-14 03:21:29 +0900

    feat(pickk-protocol): :sparkles: add SigninCompleteMessage

--- packages/pickk-protocol/lib/messages/w2a.ts
+++ packages/pickk-protocol/lib/messages/w2a.ts
@@ -1,11 +1,19 @@
 import { UserProviderType } from '@pickk/common';
 
-import { REQUEST_OAUTH_LOGIN } from '../constants';
+import { REQUEST_OAUTH_LOGIN, SIGNIN_COMPLETE } from '../constants';
 
 /** W2A */
 export type RequestLoginMessage = {
   name: typeof REQUEST_OAUTH_LOGIN;
   body: {
     oauthProvider: UserProviderType;
+  };
+};
+
+/** W2A */
+export type SigninCompleteMessage = {
+  name: typeof SIGNIN_COMPLETE;
+  body: {
+    accessToken: string;
   };
 };
